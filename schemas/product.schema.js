38203ed7ebe7161ea898254507af0a6a18c283a2
Charly_Angel <c.a.a.a.503@gmail.com>
2021-10-11 23:18:48 -0500

    feat: filtro de precios en productos

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -6,6 +6,8 @@ const price = Joi.number().integer().min(10);
 const description = Joi.string().min(8);
 const image = Joi.string().uri();
 const categoryId = Joi.number().integer();
+const price_min = Joi.number().integer();
+const price_max = Joi.number().integer();
 
 const limit = Joi.number().integer();
 const offset = Joi.number().integer();
@@ -34,6 +36,10 @@ const queryProductSchema = Joi.object({
   limit,
   offset,
   price,
+  price_max: price_max.when('price_min', {
+    is: price_min.required(),
+    then: Joi.required(),
+  }),
 });
 
 module.exports = {
