5ac9188496c892ff5ec1cce57bdc7de7ad56c885
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2023-01-17 11:28:32 -0600

    fix: typo create_at

--- db/migrations/20211012135621-init.js
+++ db/migrations/20211012135621-init.js
@@ -33,7 +33,7 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DataTypes.DATE,
-        field: 'create_at',
+        field: 'created_at',
         defaultValue: Sequelize.NOW,
       },
     });
